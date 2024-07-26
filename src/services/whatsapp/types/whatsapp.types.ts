fee1f11a97acb3eaa1215d30ca57ddbaec5e2784
Jhon Guerrero <garruxx@gmail.com>
2024-07-25 20:46:19 -0500

    feat(whatsappServices/types): :sparkles: Se crea el tipo de whatsapp callback

--- src/services/whatsapp/types/whatsapp.types.ts
+++ src/services/whatsapp/types/whatsapp.types.ts
@@ -1,7 +1,8 @@
-import { type CreateConfig } from '@wppconnect-team/wppconnect'
+import { type Whatsapp, type CreateConfig } from '@wppconnect-team/wppconnect'
 export type PuppeteerLaunchOptions = CreateConfig['puppeteerOptions']
 export interface ConnectByPIN {
 	callSing: string | number
 	phone: string | number
 }
 
+export type WhatsappCallback = (client: Whatsapp) => void
